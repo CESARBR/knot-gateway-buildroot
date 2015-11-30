bfd4959db8c31141368f0d796d50702685b438ba
Fabio Urquiza <flus@cesar.org.br>
2015-11-30 16:38:34 -0200

    Added support to knot-service-app and knot-protocol-lib

---
+++ package/knot-service-app/knot-service-app.mk
@@ -0,0 +1,15 @@
+################################################################################
+#
+# knot-service-app
+#
+################################################################################
+
+KNOT_SERVICE_APP_VERSION = app_proto
+KNOT_SERVICE_APP_SITE = https://gitlab.cesar.org.br/cesar-iot/knot-service-source.git
+KNOT_SERVICE_APP_SITE_METHOD = git
+KNOT_SERVICE_APP_INSTALL_STAGING = YES
+KNOT_SERVICE_APP_INSTALL_TARGET = NO
+KNOT_SERVICE_APP_DEPENDENCIES = libglib2 json-c libcurl knot-protocol-lib
+KNOT_SERVICE_APP_AUTORECONF = YES
+
+$(eval $(autotools-package))
