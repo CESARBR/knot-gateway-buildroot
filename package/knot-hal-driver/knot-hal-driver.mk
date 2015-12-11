1df49f58a119f483b62f6dd154dcf12378725d7c
Diogenes Vila Nova Pereira <dvnp@cesar.org.br>
2015-12-11 10:09:28 -0300

    Define the RASPBERRY constant to the compiler/preprocessor

--- package/knot-hal-driver/knot-hal-driver.mk
+++ package/knot-hal-driver/knot-hal-driver.mk
@@ -4,12 +4,12 @@
 #
 ################################################################################
 
-KNOT_HAL_DRIVER_VERSION = 4feb0086496818f87806b23fda2daa5df252f004
+KNOT_HAL_DRIVER_VERSION = c4f76d7c1b33b1988322a780af4b56e2751f4c38
 KNOT_HAL_DRIVER_SITE = https://gitlab.cesar.org.br/cesar-iot/knot-hal-source.git
 KNOT_HAL_DRIVER_SITE_METHOD = git
 KNOT_HAL_DRIVER_INSTALL_STAGING = YES
 KNOT_HAL_DRIVER_INSTALL_TARGET = NO
-#KNOT_HAL_DRIVER_DEPENDENCIES = libglib2 json-c libcurl knot-protocol-lib
 KNOT_HAL_DRIVER_AUTORECONF = YES
+KNOT_HAL_DRIVER_CONF_OPTS = CFLAGS='-DRASPBERRY'
 
 $(eval $(autotools-package))
