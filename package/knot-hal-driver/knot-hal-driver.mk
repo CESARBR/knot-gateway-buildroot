d6920b0bc69cb94a3ebc1c18f284f69c2385ff20
Everton Dornelas <everton.dornelas@cesar.org.br>
2015-12-16 14:17:22 -0300

    Build for KNOT_HAL without parallel parameter

--- package/knot-hal-driver/knot-hal-driver.mk
+++ package/knot-hal-driver/knot-hal-driver.mk
@@ -4,12 +4,13 @@
 #
 ################################################################################
 
-KNOT_HAL_DRIVER_VERSION = c4f76d7c1b33b1988322a780af4b56e2751f4c38
+KNOT_HAL_DRIVER_VERSION = dd3ad37aa11d632cd3c7acc7cefe160493bef5bf
 KNOT_HAL_DRIVER_SITE = https://gitlab.cesar.org.br/cesar-iot/knot-hal-source.git
 KNOT_HAL_DRIVER_SITE_METHOD = git
 KNOT_HAL_DRIVER_INSTALL_STAGING = YES
 KNOT_HAL_DRIVER_INSTALL_TARGET = NO
 KNOT_HAL_DRIVER_AUTORECONF = YES
+KNOT_HAL_DRIVER_MAKE = $(MAKE1)
 ifeq ($(BR2_LINUX_KERNEL_INTREE_DTS_NAME),"bcm2709-rpi-2-b")
 KNOT_HAL_DRIVER_CONF_OPTS = CFLAGS='-DRPI2_BOARD'
 else
