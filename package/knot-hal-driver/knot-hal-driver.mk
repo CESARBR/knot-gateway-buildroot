26f096f8d07123d3d21c6c32f2217780492aad7d
Diogenes Vila Nova Pereira <dvnp@cesar.org.br>
2016-03-10 11:10:55 -0300

    Updated knot-hal-driver.mk package to last commit of knot-hal

--- package/knot-hal-driver/knot-hal-driver.mk
+++ package/knot-hal-driver/knot-hal-driver.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-KNOT_HAL_DRIVER_VERSION = dd3ad37aa11d632cd3c7acc7cefe160493bef5bf
+KNOT_HAL_DRIVER_VERSION = 5a60d651f65443508ad25359672ac2f79667275f
 KNOT_HAL_DRIVER_SITE = https://gitlab.cesar.org.br/cesar-iot/knot-hal-source.git
 KNOT_HAL_DRIVER_SITE_METHOD = git
 KNOT_HAL_DRIVER_INSTALL_STAGING = YES
