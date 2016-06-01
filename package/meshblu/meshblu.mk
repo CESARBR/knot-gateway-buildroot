36966d498913d60db8d9fa581bcd54243934d20f
Fabio Urquiza <flus@cesar.org.br>
2016-06-01 16:13:04 -0300

    Added meshblu package

--- package/meshblu/meshblu.mk
+++ package/meshblu/meshblu.mk
@@ -7,16 +7,8 @@
 MESHBLU_VERSION = v1.18.4
 MESHBLU_SITE = $(call github,octoblu,meshblu,$(MESHBLU_VERSION))
 MESHBLU_DEPENDENCIES = nodejs
-#MESHBLU_
-#MESHBLU_
 
 define MESHBLU_INSTALL_TARGET_CMDS
-        #$(TARGET_MAKE_ENV) PYTHON=$(HOST_DIR)/usr/bin/python2 \
-                #$(MAKE) -C $(@D) install \
-                #DESTDIR=$(TARGET_DIR) \
-                #$(TARGET_CONFIGURE_OPTS) \
-                #PATH=$(@D)/bin:$(BR_PATH) \
-                #LD="$(TARGET_CXX)"
         $(NPM) install $(@D)
 endef
 
