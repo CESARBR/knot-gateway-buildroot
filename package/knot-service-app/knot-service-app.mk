f7d33337845051dd8f58dffba3d9a53373a069e1
Flavio Castro <flcf@cesar.org.br>
2016-08-31 14:46:44 -0300

    Changed the .mk file of knot-service-app package switching from gitlab to github repository.

--- package/knot-service-app/knot-service-app.mk
+++ package/knot-service-app/knot-service-app.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-KNOT_SERVICE_APP_VERSION = app_proto
-KNOT_SERVICE_APP_SITE = https://gitlab.cesar.org.br/cesar-iot/knot-service-source.git
+KNOT_SERVICE_APP_VERSION = f7fb18ae041df643340ff3dd346345fd878ba1dd
+KNOT_SERVICE_APP_SITE = https://github.com/CESARBR/knot-service-source.git
 KNOT_SERVICE_APP_SITE_METHOD = git
 KNOT_SERVICE_APP_INSTALL_STAGING = NO
 KNOT_SERVICE_APP_INSTALL_TARGET = YES
