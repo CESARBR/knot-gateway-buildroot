cb654ab1a3e0b4bea2575621382fae4e56bcff1e
Flavio Castro <flcf@cesar.org.br>
2016-09-02 10:04:50 -0300

    Changes on knot-fog.mk file changing tags and repository from gitlab to github.

--- package/knot-fog/knot-fog.mk
+++ package/knot-fog/knot-fog.mk
@@ -1,14 +1,14 @@
 ################################################################################
 #
-# meshblu
+# knot-fog
 #
 ################################################################################
 
-MESHBLU_VERSION = v1.18.4
-MESHBLU_SITE = $(call github,octoblu,meshblu,$(MESHBLU_VERSION))
-MESHBLU_DEPENDENCIES = nodejs
+KNOT_FOG_VERSION = afc145ce8fdc66b820801db6e1fafbe0aff4a765
+KNOT_FOG_SITE = $https://github.com/CESARBR/knot-cloud-source.git
+KNOT_FOG_DEPENDENCIES = nodejs
 
-define MESHBLU_INSTALL_TARGET_CMDS
+define KNOT_FOG_INSTALL_TARGET_CMDS
         cd $(@D) && $(NPM) install -g
 endef
 
