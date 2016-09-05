66f8a7bca4c76bd887e7331c79e2525d6ada9a3f
Fabio Urquiza <flus@cesar.org.br>
2016-09-05 10:57:23 -0300

    Moved knot deamons installation scripts to packages locations

--- package/knot-fog/knot-fog.mk
+++ package/knot-fog/knot-fog.mk
@@ -5,7 +5,8 @@
 ################################################################################
 
 KNOT_FOG_VERSION = afc145ce8fdc66b820801db6e1fafbe0aff4a765
-KNOT_FOG_SITE = $https://github.com/CESARBR/knot-cloud-source.git
+KNOT_FOG_SITE_METHOD = git
+KNOT_FOG_SITE = https://github.com/CESARBR/knot-cloud-source.git
 KNOT_FOG_DEPENDENCIES = nodejs
 
 define KNOT_FOG_INSTALL_TARGET_CMDS
