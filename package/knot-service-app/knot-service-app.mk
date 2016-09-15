31dab966fa42b40807a68f64ae8bc22f59bdcaad
Flavio Castro <flcf@cesar.org.br>
2016-09-15 09:15:51 -0300

    Did change the knot-service-app.mk file to bootstrap configure and put flags to build.

--- package/knot-service-app/knot-service-app.mk
+++ package/knot-service-app/knot-service-app.mk
@@ -4,14 +4,21 @@
 #
 ################################################################################
 
-KNOT_SERVICE_APP_VERSION = f7fb18ae041df643340ff3dd346345fd878ba1dd
+KNOT_SERVICE_APP_VERSION = b90c99f101566c150fbff7ad6a41920cee30456a
 KNOT_SERVICE_APP_SITE = https://github.com/CESARBR/knot-service-source.git
 KNOT_SERVICE_APP_SITE_METHOD = git
 KNOT_SERVICE_APP_INSTALL_STAGING = NO
 KNOT_SERVICE_APP_INSTALL_TARGET = YES
-KNOT_SERVICE_APP_DEPENDENCIES = libglib2 json-c libcurl knot-protocol-lib
+KNOT_SERVICE_APP_DEPENDENCIES = libglib2 json-c libcurl knot-protocol-lib libwebsockets
 KNOT_SERVICE_APP_AUTORECONF = YES
-KNOT_SERVICE_APP_CONF_OPTS = --prefix=/usr/local --exec-prefix=/usr/local
+KNOT_SERVICE_APP_CONF_OPTS = --prefix=/usr/local --exec-prefix=/usr/local --enable-debug --disable-optimization
+KNOT_SERVICE_APP_CONF_ENV = CFLAGS="-I$(@D)/../knot-protocol-lib-66f11aa1aeab9f47a89780cff008b86893496fb4/src" LIBS="$(@D)/../knot-protocol-lib-66f11aa1aeab9f47a89780cff008b86893496fb4/src/libknotprotocol.a"
+
+define KNOT_SERVICE_APP_BOOTSTRAP
+	cd $(@D) &&  ./bootstrap 
+endef
+
+KNOT_SERVICE_APP_PRE_CONFIGURE_HOOKS += KNOT_SERVICE_APP_BOOTSTRAP
 
 define KNOT_SERVICE_APP_INSTALL_INIT_SCRIPT
 	$(INSTALL) -D -m 0644 $(KNOT_SERVICE_APP_PKGDIR)/S60knotd-daemon $(TARGET_DIR)/etc/init.d/
