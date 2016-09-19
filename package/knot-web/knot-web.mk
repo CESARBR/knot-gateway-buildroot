387313af6683edb418981c63a0070ae9164f0869
Renie Delgado <rad@cesar.org.br>
2016-09-19 14:34:52 -0300

    Created package on Buildroot for webserver
    Created package for web server. It installs the server dependencies and
    initialize the server on rasp's boot.

---
+++ package/knot-web/knot-web.mk
@@ -0,0 +1,26 @@
+################################################################################
+#
+#knot-web
+#
+################################################################################
+
+KNOT_WEB_VERSION = dc6f7285f83c4f4373adaa2dc1a269562389ae81
+KNOT_WEB_SITE_METHOD = git
+KNOT_WEB_SITE = https://github.com/CESARBR/knot-gateway-webui.git
+KNOT_WEB_DEPENDENCIES = nodejs
+
+define KNOT_WEB_INSTALL_TARGET_CMDS
+	rm -rf $(TARGET_DIR)/usr/local/bin/knot-web*
+	mkdir -p $(TARGET_DIR)/usr/local/bin/
+	cd $(@D) && $(NPM) install
+	cp -R $(@D) $(TARGET_DIR)/usr/local/bin/
+endef
+
+define KNOT_WEB_INSTALL_INIT_SCRIPT
+	$(INSTALL) -D -m 0777 $(KNOT_WEB_PKGDIR)/knot-web $(TARGET_DIR)/usr/local/bin/
+	$(INSTALL) -D -m 0777 $(KNOT_WEB_PKGDIR)/S70knot-web-daemon $(TARGET_DIR)/etc/init.d/
+endef
+
+KNOT_WEB_POST_INSTALL_TARGET_HOOKS += KNOT_WEB_INSTALL_INIT_SCRIPT
+
+$(eval $(generic-package))
