7df4e07509961217e328cbb61595d3cf8b35c0c7
Flavio Castro <flcf@cesar.org.br>
2016-09-30 10:16:25 -0300

    Change post-build.sh script
    This patch removes "/var/log" symbolic link and switches "/var/log" to
    a directory.

--- board/raspberrypi/post-build.sh
+++ board/raspberrypi/post-build.sh
@@ -9,3 +9,9 @@ if [ -e ${TARGET_DIR}/etc/inittab ]; then
 	sed -i '/GENERIC_SERIAL/a\
 tty1::respawn:/sbin/getty -L  tty1 0 vt100 # HDMI console' ${TARGET_DIR}/etc/inittab
 fi
+
+# Remove "/var/log" link and make "/var/log" a directory
+if [ -h ${TARGET_DIR}/var/log ]; then
+	unlink ${TARGET_DIR}/var/log
+fi
+mkdir -p -m 775 ${TARGET_DIR}/var/log/
