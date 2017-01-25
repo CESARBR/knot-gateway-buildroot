40696e99cb98cb5e669b14d77d2ea6159f13e813
Flavio Castro <flcf@cesar.org.br>
2017-01-25 09:08:45 -0300

    Remove duplicated files

--- package/knot-web/knot-web.mk
+++ package/knot-web/knot-web.mk
@@ -16,6 +16,8 @@ define KNOT_WEB_INSTALL_TARGET_CMDS
 	cp -R $(@D) $(TARGET_DIR)/usr/local/bin/
 	$(INSTALL) -D -m 0644 $(@D)/app/config/gatewayConfig.json $(TARGET_DIR)/etc/knot
 	$(INSTALL) -D -m 0644 $(@D)/app/config/keys.json $(TARGET_DIR)/etc/knot
+	rm -f $(TARGET_DIR)/usr/local/bin/knot-web*/app/config/gatewayConfig.json
+	rm -f $(TARGET_DIR)/usr/local/bin/knot-web*/app/config/keys.json
 endef
 
 define KNOT_WEB_INSTALL_INIT_SCRIPT
