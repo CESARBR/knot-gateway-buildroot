f106af24ad1f8c7f2445efa6c430773d7a8f9128
Wilder Galvao <wilder.galvao@gmail.com>
2017-03-08 09:35:52 -0300

    Move mongodb configuration code from knot-fog package folder

--- package/knot-fog/knot-fog.mk
+++ package/knot-fog/knot-fog.mk
@@ -14,16 +14,12 @@ define KNOT_FOG_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/usr/local/
 	mkdir -p $(TARGET_DIR)/usr/local/bin/
 	mkdir -p $(TARGET_DIR)/usr/local/bin/knot-fog-source
-	mkdir -p $(TARGET_DIR)/data/db
 	cd $(@D) && $(NPM) install
 	cp -R $(@D)/* $(TARGET_DIR)/usr/local/bin/knot-fog-source
 endef
 
 define KNOT_FOG_INSTALL_INIT_SCRIPT
-  $(INSTALL) -D -m 0755 $(KNOT_FOG_PKGDIR)/S55knot-fog-mongo-daemon $(TARGET_DIR)/etc/init.d/
 	$(INSTALL) -D -m 0755 $(KNOT_FOG_PKGDIR)/S60knot-fog-daemon $(TARGET_DIR)/etc/init.d/
-	mkdir -p $(TARGET_DIR)/usr/local/
-	mkdir -p $(TARGET_DIR)/usr/local/bin/
 	$(INSTALL) -D -m 0755 $(KNOT_FOG_PKGDIR)/knot-fog $(TARGET_DIR)/usr/local/bin/
 endef
 
