95be1026052ce9afe63292957259d3344cd6bebd
Wilder Galvao <wilder.galvao@gmail.com>
2017-03-08 09:35:52 -0300

    Move mongodb configuration code from knot-fog package folder

--- package/mongodb/mongodb.mk
+++ package/mongodb/mongodb.mk
@@ -61,6 +61,14 @@ define MONGODB_INSTALL_TARGET_CMDS
 		$(MONGODB_SCONS_OPTS) \
 		--prefix=$(TARGET_DIR)/usr \
 		install)
+		mkdir -p $(TARGET_DIR)/data/db
+endef
+
+define MONGODB_INSTALL_INIT_SCRIPT
+	$(INSTALL) -D -m 0755 $(MONGODB_PKGDIR)/S55mongod $(TARGET_DIR)/etc/init.d/
+	mkdir -p $(TARGET_DIR)/usr/local/
+	mkdir -p $(TARGET_DIR)/usr/local/bin/
+	$(INSTALL) -D -m 0755 $(MONGODB_PKGDIR)/mongod $(TARGET_DIR)/usr/local/bin/
 endef
 
 $(eval $(generic-package))
