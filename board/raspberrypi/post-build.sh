dadc8e799c633c7344018d0968f2e5238bd58660
Paulo Serra Filho <ptvsf@cesar.org.br>
2017-06-13 15:31:38 -0300

    Create a file to show the app versions on rasp

--- board/raspberrypi/post-build.sh
+++ board/raspberrypi/post-build.sh
@@ -18,3 +18,8 @@ mkdir -p -m 775 ${TARGET_DIR}/var/log/
 
 # Change hostname
 echo "knot" > ${TARGET_DIR}/etc/hostname
+
+# Create knot version file
+echo "KNoT - KNoT Network of Things" > ${TARGET_DIR}/etc/knot/.version
+echo "Built on $(date --rfc-3339='seconds')" >> ${TARGET_DIR}/etc/knot/.version
+echo "$(grep -rhiZ ^KNOT_.*_VERSION $(pwd)/package)" >> ${TARGET_DIR}/etc/knot/.version
