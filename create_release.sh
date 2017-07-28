802fe46282016cacb28eafcefe26c87c20313ab3
Paulo Serra Filho <ptvsf@cesar.org.br>
2017-07-28 10:57:52 -0300

    create_release.sh: Fix on regex
    The replacement was removing the " when applying the tag.

--- create_release.sh
+++ create_release.sh
@@ -5,7 +5,7 @@ sed -i "/KNOT_WEB_VERSION/ s/=.*/= $TAG/g" ./package/knot-web/knot-web.mk
 sed -i "/KNOT_HAL_DRIVER_VERSION/ s/=.*/= $TAG/g" ./package/knot-hal-driver/knot-hal-driver.mk
 sed -i "/KNOT_PROTOCOL_LIB_VERSION/ s/=.*/= $TAG/g" ./package/knot-protocol-lib/knot-protocol-lib.mk
 sed -i "/KNOT_SERVICE_APP_VERSION/ s/=.*/= $TAG/g" ./package/knot-service-app/knot-service-app.mk
-sed -i "/KNOT_SERVICE_APP_CONF_ENV/ s/\(KNOT-\)[^ /]*/$TAG/g" package/knot-service-app/knot-service-app.mk
+sed -i "/KNOT_SERVICE_APP_CONF_ENV/ s/\(KNOT-\)[^ / | \"]*/$TAG/g" package/knot-service-app/knot-service-app.mk
 git add package/knot-fog/knot-fog.mk package/knot-web/knot-web.mk package/knot-hal-driver/knot-hal-driver.mk package/knot-protocol-lib/knot-protocol-lib.mk package/knot-service-app/knot-service-app.mk
 git commit
 git tag "$TAG"
