2ba20c73e81006af2c3b8c5f03b7cf069c41bdc9
Thiago Cardoso <thiago.figueredo@cesar.org.br>
2017-09-01 20:29:35 -0300

    Remove development packages from knot-web deploy

--- package/knot-web/knot-web.mk
+++ package/knot-web/knot-web.mk
@@ -12,7 +12,7 @@ KNOT_WEB_DEPENDENCIES = nodejs
 define KNOT_WEB_INSTALL_TARGET_CMDS
 	rm -rf $(TARGET_DIR)/usr/local/bin/knot-web*
 	mkdir -p $(TARGET_DIR)/usr/local/bin/
-	cd $(@D) && $(NPM) install && $(NPM) run build
+	cd $(@D) && $(NPM) install && $(NPM) run build && $(NPM) prune --production
 	cp -R $(@D) $(TARGET_DIR)/usr/local/bin/
 	$(INSTALL) -D -m 0644 $(@D)/app/config/gatewayConfig.json $(TARGET_DIR)/etc/knot
 	$(INSTALL) -D -m 0666 $(@D)/app/config/keys.json $(TARGET_DIR)/etc/knot
