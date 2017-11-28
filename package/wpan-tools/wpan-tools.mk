d032aa01eb0ae17242fc36c2caf8b7f0fa062cb0
Wilton Santana <wpss@cesar.org.br>
2017-11-28 15:47:34 -0300

    wpan-tools: Change URL to CESAR's fork
    Switch source URL to https://github.com/CESARBR/knot-wpan-tools-source.git
    in order to include security patches.

--- package/wpan-tools/wpan-tools.mk
+++ package/wpan-tools/wpan-tools.mk
@@ -4,10 +4,16 @@
 #
 ################################################################################
 
-WPAN_TOOLS_VERSION = 0.5
-WPAN_TOOLS_SITE = http://wpan.cakelab.org/releases
+WPAN_TOOLS_VERSION = nl802154_llsec
+WPAN_TOOLS_SITE = https://github.com/CESARBR/knot-wpan-tools-source.git
+WPAN_TOOLS_SITE_METHOD = git
 WPAN_TOOLS_DEPENDENCIES = host-pkgconf libnl
-WPAN_TOOLS_LICENSE = iw license
+WPAN_TOOLS_AUTORECONF = YES
+WPAN_TOOLS_AUTORECONF_OPTS = --install --symlink
+WPAN_TOOLS_CONF_OPTS = CFLAGS='-O2' --prefix=/usr --sysconfdir=/etc --libdir=/usr/lib
+WPAN_TOOLS_LICENSE = ISC
 WPAN_TOOLS_LICENSE_FILES = COPYING
+
+WPAN_TOOLS_LIBTOOL_PATCH = NO
 
 $(eval $(autotools-package))
