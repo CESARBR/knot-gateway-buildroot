ae55c28b7be6eca3a63e61211eec25a5a67c7fdb
Claudio Takahasi <ckt@cesar.org.br>
2018-02-14 13:15:17 -0300

    package: Build ELL as static library

--- package/libell/libell.mk
+++ package/libell/libell.mk
@@ -4,12 +4,26 @@
 #
 ################################################################################
 
-LIBELL_VERSION = 0.3
-LIBELL_SITE = https://git.kernel.org/pub/scm/libs/ell/ell.git
-LIBELL_SITE_METHOD = git
+LIBELL_VERSION = 0.4
+LIBELL_SOURCE = ell-$(LIBELL_VERSION).tar.gz
+LIBELL_SITE = https://git.kernel.org/pub/scm/libs/ell/ell.git/snapshot
+LIBELL_LICENSE_FILES = COPYING
+LIBELL_CFLAGS = $(TARGET_CFLAGS)
 LIBELL_AUTORECONF = YES
 LIBELL_INSTALL_STAGING = YES
 LIBELL_CONF_OPTS = --prefix=/usr
+
+ifeq ($(BR2_STATIC_LIBS),y)
+LIBELL_CONF_OPTS += \
+	--enable-shared=no \
+	--without-libdl
+else
+LIBELL_CONF_OPTS += --enable-static=no
+endif
+
+LIBELL_CONF_ENV = \
+	CFLAGS="$(LIBELL_CFLAGS)" \
+	LDFLAGS="$(TARGET_LDFLAGS) -lm"
 
 define LIBELL_BOOTSTRAP
         ln -s $(@D) $(@D)/../ell.git && cd $(@D) && ./bootstrap
