ffaf5a2f79834457f2435000bb9bd9ed7f750be5
Claudio Takahasi <ckt@cesar.org.br>
2018-02-20 10:13:09 -0300

    Add release candidate KNOT-v01.03-rc03
    Fix SPI chip selector for RPi2/RPi3. Add ipwand package: system
    daemon responsible for managing lowpan0 (802.15.4) interfaces.

--- package/knot-protocol-lib/knot-protocol-lib.mk
+++ package/knot-protocol-lib/knot-protocol-lib.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-KNOT_PROTOCOL_LIB_VERSION = KNOT-v01.03-rc02
+KNOT_PROTOCOL_LIB_VERSION = KNOT-v01.03-rc03
 KNOT_PROTOCOL_LIB_SITE = https://github.com/CESARBR/knot-protocol-source.git
 KNOT_PROTOCOL_LIB_SITE_METHOD = git
 KNOT_PROTOCOL_LIB_INSTALL_STAGING = YES
--- package/knot-service-app/knot-service-app.mk
+++ package/knot-service-app/knot-service-app.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-KNOT_SERVICE_APP_VERSION = KNOT-v01.03-rc02
+KNOT_SERVICE_APP_VERSION = KNOT-v01.03-rc03
 KNOT_SERVICE_APP_SITE = https://github.com/CESARBR/knot-service-source.git
 KNOT_SERVICE_APP_SITE_METHOD = git
 KNOT_SERVICE_APP_INSTALL_STAGING = NO
@@ -12,7 +12,7 @@ KNOT_SERVICE_APP_INSTALL_TARGET = YES
 KNOT_SERVICE_APP_DEPENDENCIES = libglib2 json-c libcurl knot-protocol-lib libwebsockets
 KNOT_SERVICE_APP_AUTORECONF = YES
 KNOT_SERVICE_APP_CONF_OPTS = --prefix=/usr/local --exec-prefix=/usr/local --enable-debug --disable-optimization
-KNOT_SERVICE_APP_CONF_ENV = CFLAGS="-I$(@D)/../knot-protocol-lib-KNOT-v01.03-rc02/src -I$(@D)/../knot-hal-driver-KNOT-v01.03-rc02" LIBS="$(@D)/../knot-protocol-lib-KNOT-v01.03-rc02/src/libknotprotocol.a $(@D)/../knot-hal-driver-KNOT-v01.03-rc02/src/hal/log/libhallog.a"
+KNOT_SERVICE_APP_CONF_ENV = CFLAGS="-I$(@D)/../knot-protocol-lib-KNOT-v01.03-rc03/src -I$(@D)/../knot-hal-driver-KNOT-v01.03-rc03" LIBS="$(@D)/../knot-protocol-lib-KNOT-v01.03-rc03/src/libknotprotocol.a $(@D)/../knot-hal-driver-KNOT-v01.03-rc03/src/hal/log/libhallog.a"
 
 define KNOT_SERVICE_APP_BOOTSTRAP
 	cd $(@D) &&  ./bootstrap
