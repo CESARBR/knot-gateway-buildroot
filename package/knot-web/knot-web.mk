cacd4bef622e02a8df0746461323b764d8ceccff
Ramon Ribeiro <rhpr@cesar.org.br>
2018-03-29 09:23:03 -0300

    Remove json files from knot-web package

--- package/knot-web/knot-web.mk
+++ package/knot-web/knot-web.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-KNOT_WEB_VERSION = KNOT-v01.03-rc03
+KNOT_WEB_VERSION = master
 KNOT_WEB_SITE_METHOD = git
 KNOT_WEB_SITE = https://github.com/CESARBR/knot-gateway-webui.git
 KNOT_WEB_DEPENDENCIES = nodejs
@@ -14,11 +14,6 @@ define KNOT_WEB_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/usr/local/bin/
 	cd $(@D) && $(NPM) install && $(NPM) run build && $(NPM) prune --production
 	cp -R $(@D) $(TARGET_DIR)/usr/local/bin/
-	mkdir -p $(TARGET_DIR)/etc/knot
-	$(INSTALL) -D -m 0644 $(@D)/config/gatewayConfig.json $(TARGET_DIR)/etc/knot
-	$(INSTALL) -D -m 0666 $(@D)/config/keys.json $(TARGET_DIR)/etc/knot
-	rm -f $(TARGET_DIR)/usr/local/bin/knot-web*/config/gatewayConfig.json
-	rm -f $(TARGET_DIR)/usr/local/bin/knot-web*/config/keys.json
 endef
 
 define KNOT_WEB_INSTALL_INIT_SCRIPT
