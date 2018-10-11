68fc7c11d8bd2ad6b76c148095a6489f325475db
João Neto <joao.alexandre@cesar.org.br>
2018-10-10 21:19:07 -0300

    Remove connector folder version suffix on target
    This patch allows to keep the fog connector directory with a static
    name instead of putting a version suffix.

--- package/knot-fog-connector/knot-fog-connector.mk
+++ package/knot-fog-connector/knot-fog-connector.mk
@@ -10,14 +10,14 @@ KNOT_FOG_CONNECTOR_SITE = https://github.com/CESARBR/knot-fog-connector.git
 KNOT_FOG_CONNECTOR_DEPENDENCIES = nodejs
 
 define KNOT_FOG_CONNECTOR_INSTALL_TARGET_CMDS
-	rm -rf $(TARGET_DIR)/usr/local/bin/knot-fog-connector*
-	mkdir -p $(TARGET_DIR)/usr/local/bin/
+	rm -rf $(TARGET_DIR)/usr/local/bin/knot-fog-connector
+	mkdir -p $(TARGET_DIR)/usr/local/bin/knot-fog-connector
 	cd $(@D) && $(NPM) install && $(NPM) run build
-	cp -R $(@D) $(TARGET_DIR)/usr/local/bin/
+	cp -R $(@D)/. $(TARGET_DIR)/usr/local/bin/knot-fog-connector
 endef
 
 define KNOT_FOG_CONNECTOR_INSTALL_INIT_SCRIPT
-	$(INSTALL) -D -m 0755 $(KNOT_FOG_CONNECTOR_PKGDIR)/knot-fog-connector $(TARGET_DIR)/usr/local/bin/
+	$(INSTALL) -D -m 0755 $(KNOT_FOG_CONNECTOR_PKGDIR)/knot-connector $(TARGET_DIR)/usr/local/bin/
 	$(INSTALL) -D -m 0755 $(KNOT_FOG_CONNECTOR_PKGDIR)/S80knot-fog-connector-daemon $(TARGET_DIR)/etc/init.d/
 endef
 
