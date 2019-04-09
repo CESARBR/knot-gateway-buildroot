fe00e334815b327c824298fb294031c71ef3205e
Paulo Serra Filho <ptvsf@cesar.org.br>
2019-04-08 21:20:19 -0300

    create_release: Clone KNoT repositories

--- create_release.sh
+++ create_release.sh
@@ -1,4 +1,46 @@
 #!/bin/bash
+TMP_PATH=tmp
+
+GITHUB_SSH=git@github.com:
+
+KNOT_ORG=CESARBR
+KNOT_REPOS=(knot-cloud-source knot-gateway-webui \
+knot-hal-source \
+knot-protocol-source \
+knot-service-source \
+knot-fog-connector \
+knot-network-nrf24 \
+knot-cloud)
+
+function continueIfYes() {
+    echo "Do you want to continue? [y|N]"
+    read resp
+    if [ "$resp" != "y" ];
+    then
+        if [ "$resp" != "Y" ];
+        then
+            exit 0
+        fi
+    fi
+}
+
+if [ -d "$TMP_PATH" ]
+then
+    echo "A directory named $TMP_PATH already exists."
+    echo "If you continue this will erase the existing directory."
+    continueIfYes
+    rm -rf "$TMP_PATH"
+fi
+
+mkdir -p "$TMP_PATH" && cd "$TMP_PATH"
+
+for repo in ${KNOT_REPOS[@]}; do
+    git clone "${GITHUB_SSH}${KNOT_ORG}/${repo}"
+    if [ ! $? -eq 0 ]; then
+        exit 1
+    fi
+done
+
 read -p "Tag to apply: " TAG
 sed -i "/KNOT_FOG_VERSION/ s/=.*/= $TAG/g" ./package/knot-fog/knot-fog.mk
 sed -i "/KNOT_FOG_CONNECTOR_VERSION/ s/=.*/= $TAG/g" ./package/knot-fog-connector/knot-fog-connector.mk
