87477b4b287f0b48e0b7419ae9f77dc9d72a2099
Vitor Barros <vba@cesar.org.br>
2019-04-17 12:10:30 -0300

    knot-netsetup: Install daemon script on target
    Signed-off-by: Vitor Barros <vba@cesar.org.br>

--- package/knot-netsetup/knot-netsetup.mk
+++ package/knot-netsetup/knot-netsetup.mk
@@ -9,4 +9,10 @@ KNOT_NETSETUP_SITE = https://github.com/CESARBR/knot-gateway-netsetup.git
 KNOT_NETSETUP_SITE_METHOD = git
 KNOT_NETSETUP_SETUP_TYPE = setuptools
 
+define KNOT_NETSETUP_INSTALL_INIT_SCRIPT
+	$(INSTALL) -D -m 0755 $(KNOT_NETSETUP_PKGDIR)/S85netsetup-daemon $(TARGET_DIR)/etc/init.d/
+endef
+
+KNOT_NETSETUP_POST_INSTALL_TARGET_HOOKS += KNOT_NETSETUP_INSTALL_INIT_SCRIPT
+
 $(eval $(python-package))
