b66f49908a83d5495dcf3913599c126529d09699
Paulo Serra Filho <ptvsf@cesar.org.br>
2019-06-22 19:07:52 -0300

    Remove KNoT code from board/raspberrypi
    KNoT boards are under knot/ directory. Returning the raspberrypi
    directory to its original state.

--- board/raspberrypi/post-build.sh
+++ board/raspberrypi/post-build.sh
@@ -9,17 +9,3 @@ if [ -e ${TARGET_DIR}/etc/inittab ]; then
 	sed -i '/GENERIC_SERIAL/a\
 tty1::respawn:/sbin/getty -L  tty1 0 vt100 # HDMI console' ${TARGET_DIR}/etc/inittab
 fi
-
-# Remove "/var/log" link and make "/var/log" a directory
-if [ -h ${TARGET_DIR}/var/log ]; then
-	unlink ${TARGET_DIR}/var/log
-fi
-mkdir -p -m 775 ${TARGET_DIR}/var/log/
-
-# Create knot version file
-echo "KNoT - KNoT Network of Things" > ${TARGET_DIR}/etc/knot/.version
-echo "Built on $(date --rfc-3339='seconds')" >> ${TARGET_DIR}/etc/knot/.version
-echo "$(grep -rhiZ ^KNOT_.*_VERSION $(pwd)/package)" >> ${TARGET_DIR}/etc/knot/.version
-
-# Create alias
-echo "alias knot='cat /etc/knot/.version'" >> ${TARGET_DIR}/etc/profile
