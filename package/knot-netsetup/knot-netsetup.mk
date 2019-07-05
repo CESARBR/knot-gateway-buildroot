09f3037993fc2590b978f5d95a818d3e77ac4c30
Vitor Barros <vba@cesar.org.br>
2019-07-05 09:49:05 -0300

    package: netsetup: Install dbus configuration file
    Signed-off-by: Vitor Barros <vba@cesar.org.br>

--- package/knot-netsetup/knot-netsetup.mk
+++ package/knot-netsetup/knot-netsetup.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-KNOT_NETSETUP_VERSION = 38d747f445377f45e4a858d9d252282f631372a0
+KNOT_NETSETUP_VERSION = d911e2baa08815f9c76c8cf2a99ef400cda4e0a3
 KNOT_NETSETUP_SITE = https://github.com/CESARBR/knot-gateway-netsetup.git
 KNOT_NETSETUP_SITE_METHOD = git
 KNOT_NETSETUP_SETUP_TYPE = setuptools
@@ -12,6 +12,7 @@ KNOT_NETSETUP_SETUP_TYPE = setuptools
 define KNOT_NETSETUP_INSTALL_INIT_SCRIPT
 	ln -fs /usr/bin/netsetup $(TARGET_DIR)/usr/local/bin/netsetup
 	$(INSTALL) -D -m 0755 $(KNOT_NETSETUP_PKGDIR)/S85netsetup-daemon $(TARGET_DIR)/etc/init.d/
+	$(INSTALL) -D -m 0644 $(@D)/netsetup.conf $(TARGET_DIR)/etc/dbus-1/system.d/
 endef
 
 KNOT_NETSETUP_POST_INSTALL_TARGET_HOOKS += KNOT_NETSETUP_INSTALL_INIT_SCRIPT
