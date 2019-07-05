9c6b120e9c29425e50556b415acb3d1a747fffa6
Allysson Lukas <alla@cesar.org.br>
2019-07-04 22:47:50 -0300

    package: Add python-pymongo package
    Add PyMongo python package. The PyMongo is distribution containing
    tools for working with MongoDB database from Python.

---
+++ package/python-pymongo/python-pymongo.mk
@@ -0,0 +1,14 @@
+################################################################################
+#
+# python-pymongo
+#
+################################################################################
+
+PYTHON_PYMONGO_VERSION = 3.8.0
+PYTHON_PYMONGO_SOURCE = pymongo-$(PYTHON_PYMONGO_VERSION).tar.gz
+PYTHON_PYMONGO_SITE = https://files.pythonhosted.org/packages/19/77/da358f5729ff046ceaf6c6a86755f9d8285719f93df6da15bb2440367d7e
+PYTHON_PYMONGO_LICENSE = Apache-2.0
+PYTHON_PYMONGO_LICENSE_FILES = LICENSE
+PYTHON_PYMONGO_SETUP_TYPE = setuptools
+
+$(eval $(python-package))
