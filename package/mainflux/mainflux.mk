7a9879ed08d03d3568b9e94206d9901479ce339b
João Neto <j.alexandreeneto@gmail.com>
2019-11-07 09:42:41 -0300

    Add mainflux users package
    The mainflux users service will be used to support users/apps
    capabilities on the new version of the KNoT Fog. Thus, developers
    will be able to operate locally on registered things.

---
+++ package/mainflux/mainflux.mk
@@ -0,0 +1,30 @@
+################################################################################
+#
+# MAINFLUX
+#
+################################################################################
+
+MAINFLUX_VERSION = master
+MAINFLUX_SITE = $(call github,mainflux,mainflux,$(MAINFLUX_VERSION))
+MAINFLUX_LICENSE = Apache-2.0
+MAINFLUX_LICENSE_FILES = LICENSE
+
+MAINFLUX_BUILD_TARGETS = cmd/users
+
+define MAINFLUX_INSTALL_INIT_SCRIPT
+	$(INSTALL) -D -m 0755 $(MAINFLUX_PKGDIR)/S95mainflux-users $(TARGET_DIR)/etc/init.d/
+endef
+
+define MAINFLUX_INSTALL_TARGET_CMDS
+	mkdir -p $(TARGET_DIR)/usr/local/mainflux/bin
+	cp $(MAINFLUX_PKGDIR)/mainflux.config $(TARGET_DIR)/usr/local/mainflux/
+	$(INSTALL) -D -m 0755 $(@D)/bin/users $(TARGET_DIR)/usr/local/mainflux/bin/
+endef
+
+MAINFLUX_POST_INSTALL_TARGET_HOOKS += MAINFLUX_INSTALL_INIT_SCRIPT
+
+define MAINFLUX_USERS
+	mainflux -1 mainflux -1 * - /bin/sh - mainflux service daemons
+endef
+
+$(eval $(golang-package))
