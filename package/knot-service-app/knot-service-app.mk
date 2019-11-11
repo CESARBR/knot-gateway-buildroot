70b9d032380ab3d7879dba1f43b9da30f022eb0c
Ramon Ribeiro <rhpr@cesar.org.br>
2019-11-11 12:58:35 -0300

    Add release KNOT-v02.01-rc02
    This release removes the package fog from knot boards and fixes a
    security vunerability in user permission, for more details see the
    commit ac0742ae65f6479655ade83bf1b465797ed6c360.

--- package/knot-service-app/knot-service-app.mk
+++ package/knot-service-app/knot-service-app.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-KNOT_SERVICE_APP_VERSION = 8018d04fc58050af7764c36f0e315eca0e4cda0a
+KNOT_SERVICE_APP_VERSION = KNOT-v02.01-rc02
 KNOT_SERVICE_APP_SITE = https://github.com/CESARBR/knot-service-source.git
 KNOT_SERVICE_APP_SITE_METHOD = git
 KNOT_SERVICE_APP_INSTALL_STAGING = NO
