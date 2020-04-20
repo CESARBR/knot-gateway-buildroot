fd84288a424b81b164b3f3af2fbf27cec19ad422
Allysson Lukas <alla@cesar.org.br>
2020-04-20 14:21:22 -0300

    package: Add knot-virtualthing
    The KNoT VirtualThing package virtualizes a KNoT Device providing an
    abstraction to allow certain protocols to interact with a cloud.

---
+++ package/knot-virtualthing/knot-virtualthing.mk
@@ -0,0 +1,51 @@
+################################################################################
+#
+# knot-virtualthing
+#
+################################################################################
+
+KNOT_VIRTUALTHING_VERSION = KNOT-v03.00-rc01
+KNOT_VIRTUALTHING_SITE = https://github.com/CESARBR/knot-virtualthing.git
+KNOT_VIRTUALTHING_SITE_METHOD = git
+KNOT_VIRTUALTHING_INSTALL_STAGING = NO
+KNOT_VIRTUALTHING_INSTALL_TARGET = YES
+KNOT_VIRTUALTHING_DEPENDENCIES = libell json-c rabbitmq-c knot-protocol-lib \
+				libmodbus
+KNOT_VIRTUALTHING_AUTORECONF = YES
+KNOT_VIRTUALTHING_CONF_OPTS = --prefix=/usr/local --exec-prefix=/usr/local \
+			       --sysconfdir=/etc
+
+ifeq ($(BR2_ENABLE_DEBUG),y)
+KNOT_VIRTUALTHING_CONF_OPTS += \
+	--enable-debug --disable-optimization
+else
+KNOT_VIRTUALTHING_CONF_OPTS += \
+	--disable-debug --enable-optimization
+endif
+
+define KNOT_VIRTUALTHING_BOOTSTRAP
+	cd $(@D) && ./bootstrap
+endef
+
+KNOT_VIRTUALTHING_POST_PATCH_HOOKS += KNOT_VIRTUALTHING_BOOTSTRAP
+
+define KNOT_VIRTUALTHING_INSTALL_CONF_FILES
+	mkdir -p $(TARGET_DIR)/etc/knot/
+	$(INSTALL) -D -m 0644 $(@D)/confs/credentials.conf \
+		$(TARGET_DIR)/etc/knot/
+	$(INSTALL) -D -m 0644 $(@D)/confs/device.conf $(TARGET_DIR)/etc/knot/
+	$(INSTALL) -D -m 0644 $(@D)/confs/rabbitmq.conf $(TARGET_DIR)/etc/knot/
+endef
+
+KNOT_VIRTUALTHING_POST_INSTALL_TARGET_HOOKS += \
+	KNOT_VIRTUALTHING_INSTALL_CONF_FILES
+
+define KNOT_VIRTUALTHING_INSTALL_INIT_SCRIPT
+	$(INSTALL) -D -m 0755 $(KNOT_VIRTUALTHING_PKGDIR)/S65thingd-daemon \
+		$(TARGET_DIR)/etc/init.d/
+endef
+
+KNOT_VIRTUALTHING_POST_INSTALL_TARGET_HOOKS += \
+	KNOT_VIRTUALTHING_INSTALL_INIT_SCRIPT
+
+$(eval $(autotools-package))
