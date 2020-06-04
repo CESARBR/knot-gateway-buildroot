774ab28ff95eded42d0b4c7fa3a3a904fed68f5f
João Neto <joao.alexandre@cesar.org.br>
2020-06-04 11:11:06 -0300

    Rename mainflux users configuration file
    This patch updates the mainflux users configuration file from `mainflux`
    to `users` because new services can be added and each of them requires a
    specific configuration file.

--- package/mainflux/mainflux.mk
+++ package/mainflux/mainflux.mk
@@ -17,7 +17,7 @@ endef
 
 define MAINFLUX_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/usr/local/mainflux/bin
-	cp $(MAINFLUX_PKGDIR)/mainflux.config $(TARGET_DIR)/usr/local/mainflux/
+	cp $(MAINFLUX_PKGDIR)/users.conf $(TARGET_DIR)/usr/local/mainflux/
 	$(INSTALL) -D -m 0755 $(@D)/bin/users $(TARGET_DIR)/usr/local/mainflux/bin/
 endef
 
