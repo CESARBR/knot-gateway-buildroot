93bb1c53188cc3058dae4e31e860fe68769dae9a
João Neto <joao.alexandre@cesar.org.br>
2020-06-04 11:20:14 -0300

    Add knot-babeltower package
    The babeltower service orchestrates the communication with the fog
    services, acting both as a proxy and a protocol translator since the
    internal operations can be made to services that support different
    application protocols. The devices that support IP connectivity can
    connect directly to it over AMQP and JSON technologies.

---
+++ package/knot-babeltower/knot-babeltower.mk
@@ -0,0 +1,21 @@
+KNOT_BABELTOWER_VERSION = KNOT-v03.00-rc01
+KNOT_BABELTOWER_SITE = $(call github,cesarbr,knot-babeltower,$(KNOT_BABELTOWER_VERSION))
+KNOT_BABELTOWER_LICENSE = Apache-2.0
+KNOT_BABELTOWER_LICENSE_FILES = LICENSE
+
+KNOT_BABELTOWER_BUILD_TARGETS = cmd
+
+define KNOT_BABELTOWER_INSTALL_INIT_SCRIPT
+	$(INSTALL) -D -m 0755 $(KNOT_BABELTOWER_PKGDIR)/S100knot-babeltower $(TARGET_DIR)/etc/init.d/
+endef
+
+define KNOT_BABELTOWER_INSTALL_TARGET_CMDS
+	mkdir -p $(TARGET_DIR)/usr/local/babeltower/internal
+	cp -R $(@D)/internal $(TARGET_DIR)/usr/local/babeltower
+	cp $(KNOT_BABELTOWER_PKGDIR)/babeltower.conf $(TARGET_DIR)/usr/local/babeltower/
+	$(INSTALL) -D -m 0755 $(@D)/bin/cmd $(TARGET_DIR)/usr/local/babeltower/babeltower
+endef
+
+KNOT_BABELTOWER_POST_INSTALL_TARGET_HOOKS += KNOT_BABELTOWER_INSTALL_INIT_SCRIPT
+
+$(eval $(golang-package))
