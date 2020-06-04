9f9af1d100bc3b73489082de16d6f83855a319fb
João Neto <joao.alexandre@cesar.org.br>
2020-06-04 11:16:04 -0300

    Add mainflux authn service
    The mainflux authn service will be responsible for managing the users
    and apps authentication on the new KNoT Fog solution.

--- package/mainflux/mainflux.mk
+++ package/mainflux/mainflux.mk
@@ -9,19 +9,22 @@ MAINFLUX_SITE = $(call github,mainflux,mainflux,$(MAINFLUX_VERSION))
 MAINFLUX_LICENSE = Apache-2.0
 MAINFLUX_LICENSE_FILES = LICENSE
 
-MAINFLUX_BUILD_TARGETS = cmd/users cmd/things
+MAINFLUX_BUILD_TARGETS = cmd/users cmd/things cmd/authn
 
 define MAINFLUX_INSTALL_INIT_SCRIPT
 	$(INSTALL) -D -m 0755 $(MAINFLUX_PKGDIR)/S95mainflux-users $(TARGET_DIR)/etc/init.d/
 	$(INSTALL) -D -m 0755 $(MAINFLUX_PKGDIR)/S95mainflux-things $(TARGET_DIR)/etc/init.d/
+	$(INSTALL) -D -m 0755 $(MAINFLUX_PKGDIR)/S95mainflux-authn $(TARGET_DIR)/etc/init.d/
 endef
 
 define MAINFLUX_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/usr/local/mainflux/bin
 	cp $(MAINFLUX_PKGDIR)/users.conf $(TARGET_DIR)/usr/local/mainflux/
 	cp $(MAINFLUX_PKGDIR)/things.conf $(TARGET_DIR)/usr/local/mainflux/
+	cp $(MAINFLUX_PKGDIR)/authn.conf $(TARGET_DIR)/usr/local/mainflux/
 	$(INSTALL) -D -m 0755 $(@D)/bin/users $(TARGET_DIR)/usr/local/mainflux/bin/
 	$(INSTALL) -D -m 0755 $(@D)/bin/things $(TARGET_DIR)/usr/local/mainflux/bin/
+	$(INSTALL) -D -m 0755 $(@D)/bin/authn $(TARGET_DIR)/usr/local/mainflux/bin/
 endef
 
 MAINFLUX_POST_INSTALL_TARGET_HOOKS += MAINFLUX_INSTALL_INIT_SCRIPT
