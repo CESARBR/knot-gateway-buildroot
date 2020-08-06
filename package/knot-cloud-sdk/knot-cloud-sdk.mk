18dcea818493d3940105b8104e38e29bf0677031
Vitor Barros <vba@cesar.org.br>
2020-08-06 11:16:09 -0300

    package: Add knot-cloud-sdk-c
    KNoT Cloud SDK in C is part of the KNoT project. It is a client-side
    library that provides an AMQP abstraction to the KNoT Cloud for C
    applications.

---
+++ package/knot-cloud-sdk/knot-cloud-sdk.mk
@@ -0,0 +1,30 @@
+################################################################################
+#
+# knot-cloud-sdk
+#
+################################################################################
+
+KNOT_CLOUD_SDK_VERSION = 1bdb2dd9800b4840d3152ca0435497e115f995ff
+KNOT_CLOUD_SDK_SITE = https://github.com/CESARBR/knot-cloud-sdk-c.git
+KNOT_CLOUD_SDK_SITE_METHOD = git
+KNOT_CLOUD_SDK_INSTALL_STAGING = YES
+KNOT_CLOUD_SDK_INSTALL_TARGET = YES
+KNOT_CLOUD_SDK_AUTORECONF = YES
+KNOT_CLOUD_SDK_DEPENDENCIES = libell json-c rabbitmq-c knot-protocol-lib
+KNOT_CLOUD_SDK_CONF_OPTS = --sysconfdir=/etc
+
+ifeq ($(BR2_ENABLE_DEBUG),y)
+KNOT_CLOUD_SDK_CONF_OPTS += \
+	--enable-debug --disable-optimization
+else
+KNOT_CLOUD_SDK_CONF_OPTS += \
+	--disable-debug --enable-optimization
+endif
+
+define KNOT_CLOUD_SDK_BOOTSTRAP
+	cd $(@D) && ./bootstrap
+endef
+
+KNOT_CLOUD_SDK_POST_PATCH_HOOKS += KNOT_CLOUD_SDK_BOOTSTRAP
+
+$(eval $(autotools-package))
