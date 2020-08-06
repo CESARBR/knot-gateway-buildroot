4947d00fe3aa961f47058ca7cd702692c5d2dcac
Vitor Barros <vba@cesar.org.br>
2020-08-06 11:57:10 -0300

    package: virtualthing: Update to use Cloud SDK

--- package/knot-virtualthing/knot-virtualthing.mk
+++ package/knot-virtualthing/knot-virtualthing.mk
@@ -4,12 +4,12 @@
 #
 ################################################################################
 
-KNOT_VIRTUALTHING_VERSION = KNOT-v03.00-rc01
+KNOT_VIRTUALTHING_VERSION = 9f1f46bfb13a5a614da50f09a2bda416b5d0e656
 KNOT_VIRTUALTHING_SITE = https://github.com/CESARBR/knot-virtualthing.git
 KNOT_VIRTUALTHING_SITE_METHOD = git
 KNOT_VIRTUALTHING_INSTALL_STAGING = NO
 KNOT_VIRTUALTHING_INSTALL_TARGET = YES
-KNOT_VIRTUALTHING_DEPENDENCIES = libell json-c rabbitmq-c knot-protocol-lib \
+KNOT_VIRTUALTHING_DEPENDENCIES = libell knot-cloud-sdk knot-protocol-lib \
 				libmodbus
 KNOT_VIRTUALTHING_AUTORECONF = YES
 KNOT_VIRTUALTHING_CONF_OPTS = --prefix=/usr/local --exec-prefix=/usr/local \
@@ -34,7 +34,7 @@ define KNOT_VIRTUALTHING_INSTALL_CONF_FILES
 	$(INSTALL) -D -m 0644 $(@D)/confs/credentials.conf \
 		$(TARGET_DIR)/etc/knot/
 	$(INSTALL) -D -m 0644 $(@D)/confs/device.conf $(TARGET_DIR)/etc/knot/
-	$(INSTALL) -D -m 0644 $(@D)/confs/rabbitmq.conf $(TARGET_DIR)/etc/knot/
+	$(INSTALL) -D -m 0644 $(@D)/confs/cloud.conf $(TARGET_DIR)/etc/knot/
 endef
 
 KNOT_VIRTUALTHING_POST_INSTALL_TARGET_HOOKS += \
