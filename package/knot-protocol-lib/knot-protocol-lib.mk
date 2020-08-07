011366fee1a8b4a472590c7574c3910985855d41
Vitor Barros <vba@cesar.org.br>
2020-08-06 21:15:44 -0300

    package: protocol: Fix wrong macros on mk file

--- package/knot-protocol-lib/knot-protocol-lib.mk
+++ package/knot-protocol-lib/knot-protocol-lib.mk
@@ -9,14 +9,13 @@ KNOT_PROTOCOL_LIB_SITE = https://github.com/CESARBR/knot-protocol-source.git
 KNOT_PROTOCOL_LIB_SITE_METHOD = git
 KNOT_PROTOCOL_LIB_INSTALL_STAGING = YES
 KNOT_PROTOCOL_LIB_INSTALL_TARGET = YES
-KNOT_SERVICE_APP_AUTORECONF = YES
-KNOT_SERVICE_APP_CONF_OPTS = --prefix=/usr/local --exec-prefix=/usr/local --sysconfdir=/etc
+KNOT_PROTOCOL_LIB_AUTORECONF = YES
 
 ifeq ($(BR2_ENABLE_DEBUG),y)
-KNOT_SERVICE_APP_CONF_OPTS += \
+KNOT_PROTOCOL_LIB_CONF_OPTS += \
 	--enable-debug --disable-optimization
 else
-KNOT_SERVICE_APP_CONF_OPTS += \
+KNOT_PROTOCOL_LIB_CONF_OPTS += \
 	--disable-debug --enable-optimization
 endif
 
